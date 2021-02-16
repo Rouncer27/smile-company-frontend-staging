47115816173500733f42618850a3ab978e8e167b
Rouncer27 <trevor@switchbackcreative.ca>
2021-02-16 10:18:36 -0700

    finished the pricing section

--- src/components/PageComponents/Pricing.js
+++ src/components/PageComponents/Pricing.js
@@ -53,16 +53,48 @@ const Pricing = ({ data }) => {
           <Link to={`/${data.buttonSlug}`}>{data.buttonText}</Link>
         </div>
       </div>
+      <div className="backgroundEffect">
+        <div className="backgroundEffect__inner" />
+      </div>
     </PricingStyled>
   )
 }
 
 const PricingStyled = styled.section`
+  position: relative;
   background-color: rgba(208, 204, 202, 0.25);
 
   .wrapper {
     ${standardWrapper};
+    position: relative;
     max-width: 100rem !important;
+    z-index: 10;
+  }
+
+  .backgroundEffect {
+    position: absolute;
+    top: 30%;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: ${colors.white};
+    z-index: 1;
+
+    &__inner {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      background: linear-gradient(
+        to left top,
+        rgba(173, 137, 166, 0.4) 0%,
+        rgba(173, 137, 166, 0.4) 50%,
+        rgba(173, 137, 166, 0.7) 50%,
+        rgba(173, 137, 166, 0.7) 100%
+      );
+      z-index: 10;
+    }
   }
 
   .mainTitle {
