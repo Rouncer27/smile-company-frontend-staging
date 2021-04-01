63b3051c38ab353717aeb6e414d43294ff68a201
Rouncer27 <trevor@switchbackcreative.ca>
2021-04-01 09:28:08 -0600

    added magic.link as login jwt creator

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,12 +25,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        name: `smile-and-company`,
+        short_name: `Smile and Co.`,
+        start_url: `/app/login`,
+        background_color: `#6b516d`,
+        theme_color: `#ad89a6`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
@@ -49,8 +49,6 @@ module.exports = {
         singleTypes: [],
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
