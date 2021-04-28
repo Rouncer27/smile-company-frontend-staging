d858e91af62f914a764e0786e57a3b1742ae8a69
Rouncer27 <trevor@switchbackcreative.ca>
2021-04-28 13:53:53 -0600

    gatsby offline settings

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,13 @@ import { UserContextProvider } from "./src/context/UserContext"
 export const wrapRootElement = ({ element }) => {
   return <UserContextProvider>{element}</UserContextProvider>
 }
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
